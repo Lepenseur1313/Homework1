2d27a05cbd073a8e3b512c6c2adfa77cc60befb0
Mesud Tekbaş <lepenseurgame@outlook.com>
2021-01-26 09:03:44 +0300

    Add project files.

---
+++ Homework1/CustomerManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    public class CustomerManager
+    {
+        Customer musteri = new Customer();
+        public void List(Customer[] customer)
+        {
+
+            foreach (Customer item in customer)
+            {
+                Console.WriteLine("Müşteri Adı :" + musteri.Ad);
+                Console.WriteLine("Müsteri Soyadı :" + musteri.Soyad);
+            }
+        }
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Eklendi");
+        }
+        public void Update(Customer customer)
+        {
+            Console.WriteLine("Güncellendi");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Silindi");
+        }
+    }
+}
---
+++ Homework1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Ad = "Ahmet";
+            customer.Soyad = "Kemal";
+
+            Customer customer1 = new Customer { Ad = "Ali", Soyad = "Cemal" };
+
+            Customer[] a = new Customer[2] { customer, customer1 };
+
+
+            CustomerManager manager = new CustomerManager();
+            manager.Add(customer);
+            manager.Update(customer1);
+            manager.List(a);
+            manager.Delete(customer1);
+            Console.ReadLine();
+        }
+    }
+}
